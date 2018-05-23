3b465bcff860b9dca1ab67d67affcc0c7ab695c3
Varun S <varun.saxena101@gmail.com>
2018-05-23 15:42:07 -0400

    adding existing files

---
+++ content.js
@@ -0,0 +1,26 @@
+document.addEventListener("mouseup", highlight);
+
+function myAlert() {
+	alert("This mouseup event listener works!");
+}
+
+function doSomething() {
+	if (document.getSelection() != '') {
+		document.body.style.background = 'yellow';
+	} else {
+		document.body.style.background = 'white';
+	}
+}
+
+function highlight() {
+	var selection = document.getSelection();
+	if (selection.toString() !== "") {
+		var range = selection.getRangeAt(0);
+		var span = document.createElement("span");
+		span.className = "highlight";
+		span.style.background = 'yellow';
+		span.innerHTML = range.toString();
+		range.deleteContents();
+		range.insertNode(span);
+	}
+}
